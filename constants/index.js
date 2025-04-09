9b972d1f49a2401d6dc026dcf5b760a17b9f0e38
Cesar Martel <cesar.martel9@gmail.com>
2025-04-09 17:00:57 +0200

    fix: schema issue

---
+++ constants/index.js
@@ -0,0 +1,3 @@
+
+export const supabaseUrl = "https://zoelblemfnxmaatopyst.supabase.co";
+export const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpvZWxibGVtZm54bWFhdG9weXN0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTkwNTAxODIsImV4cCI6MjAzNDYyNjE4Mn0.EsCOLcBllKdi0KSb1UJOLUEn0if9iLJdrTAxtyOA8t0';
